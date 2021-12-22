b693fe1a7b50e771872db093dc5a99866e5c893a
marcelpinotti <marcel.pinotti@gmail.com>
2021-12-22 10:56:22 -0300

    inserindo apenas os projetos de estudo spring padrao mvc

--- Gerenciamento_de_Cadastro_de_Pessoas/src/main/java/br/com/marcelpinotti/cadastro_de_clientes_api/repository/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.marcelpinotti.cadastro_de_clientes_api.repository;
-
-import br.com.marcelpinotti.cadastro_de_clientes_api.entity.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
